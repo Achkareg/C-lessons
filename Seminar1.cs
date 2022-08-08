04ac071c646665229aa214cf3bbd6c5723362f2f
Artem <103936427+Achkareg@users.noreply.github.com>
2022-08-08 08:53:36 +0400

    Update Seminar1.cs

--- Seminar1.cs
+++ Seminar1.cs
@@ -14,7 +14,7 @@ if (num1>num2)
 /*
 Console.Write("Введите первое число ");
 int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("ВВведите второе число: ");
+Console.Write("Введите второе число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите третье число ");
 int num3 = Convert.ToInt32(Console.ReadLine());
